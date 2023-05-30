ff25f1752e2d1f0b7ae8f5ce62ed77aec2caa2fb
Ministry of Feeding <130564319+Keuneki@users.noreply.github.com>
2023-05-30 15:04:18 +0300

    Add files via upload

--- js/albums-script.js
+++ js/albums-script.js
@@ -2,19 +2,35 @@ import { getServerData } from './fetch-module.js';
 import { mainNavMenu } from './nav-module.js';
 import { doStrFirstCapitalize } from './function-module.js';
 
+const containerElement = document.querySelector('.container');
+const pageLinksElement = document.createElement('div');
+pageLinksElement.classList.add('page-links');
+
+let currentPage = 1;
+let totalAlbums = 0;
+let totalPages = 0;
+let albumsPerPage = 25;
+
 async function getAlbumsWithPhotosAndUser() {
   const dataForm = 'albums?_expand=user&_embed=photos';
   const albums = await getServerData(dataForm);
+  totalAlbums = albums.length;
+  totalPages = Math.ceil(totalAlbums / albumsPerPage);
+
   renderView(albums);
+  renderPageLinks();
 }
 
 function renderView(albums) {
-  const containerElement = document.querySelector('.container');
   const ulElement = document.createElement('ul');
   ulElement.classList.add('list', 'album-list');
   containerElement.appendChild(ulElement);
 
-  albums.forEach(album => {
+  const startIndex = (currentPage - 1) * albumsPerPage;
+  const endIndex = currentPage * albumsPerPage;
+
+  for (let i = startIndex; i < endIndex && i < totalAlbums; i++) {
+    const album = albums[i];
     const liElement = document.createElement('li');
     const divElement = document.createElement('div');
     divElement.classList.add('album-content');
@@ -44,8 +60,83 @@ function renderView(albums) {
     divElement.appendChild(aAuthorElement);
     liElement.appendChild(divElement);
     ulElement.appendChild(liElement);
-  });
+  }
 }
+
+function renderPageLinks() {
+  pageLinksElement.innerHTML = '';
+
+  const firstPageLink = createPageLink('First', 1);
+  const previousPageLink = createPageLink('Previous', currentPage - 1);
+  const nextPageLink = createPageLink('Next', currentPage + 1);
+  const lastPageLink = createPageLink('Last', totalPages);
+
+  pageLinksElement.appendChild(firstPageLink);
+  pageLinksElement.appendChild(previousPageLink);
+
+  for (let i = 1; i <= totalPages; i++) {
+    const pageLink = createPageLink(i, i);
+    pageLinksElement.appendChild(pageLink);
+  }
+
+  pageLinksElement.appendChild(nextPageLink);
+  pageLinksElement.appendChild(lastPageLink);
+
+  containerElement.appendChild(pageLinksElement);
+}
+
+function createPageLink(text, page) {
+  const buttonElement = document.createElement('button');
+  buttonElement.textContent = text;
+
+  if (page === currentPage) {
+    buttonElement.classList.add('current-page');
+    buttonElement.disabled = true;
+  } else if (page >= 1 && page <= totalPages) {
+    buttonElement.classList.add('page-link');
+    buttonElement.addEventListener('click', () => {
+      currentPage = page;
+      containerElement.innerHTML = '';
+      getAlbumsWithPhotosAndUser();
+      renderPageLinks();
+    });
+  } else {
+    buttonElement.classList.add('disabled');
+    buttonElement.disabled = true;
+  }
+
+  return buttonElement;
+}
+
+function handleAlbumsPerPageChange(event) {
+  albumsPerPage = parseInt(event.target.value);
+  currentPage = 1;
+  containerElement.innerHTML = '';
+  getAlbumsWithPhotosAndUser();
+}
+
+const albumsPerPageSelect = document.createElement('select');
+albumsPerPageSelect.addEventListener('change', handleAlbumsPerPageChange);
+const options = [10, 25, 50, 100];
+options.forEach(option => {
+  const optionElement = document.createElement('option');
+  optionElement.value = option;
+  optionElement.textContent = option;
+  if (option === albumsPerPage) {
+    optionElement.selected = true;
+  }
+  albumsPerPageSelect.appendChild(optionElement);
+});
+
+const albumsPerPageLabel = document.createElement('label');
+albumsPerPageLabel.textContent = 'Albums per page:';
+albumsPerPageLabel.appendChild(albumsPerPageSelect);
+
+const pageControlsElement = document.createElement('div');
+pageControlsElement.classList.add('page-controls');
+pageControlsElement.appendChild(albumsPerPageLabel);
+
+containerElement.appendChild(pageControlsElement);
 
 getAlbumsWithPhotosAndUser();
 mainNavMenu();
--- js/posts-script.js
+++ js/posts-script.js
@@ -2,6 +2,26 @@ import { getServerData } from './fetch-module.js';
 import { mainNavMenu } from './nav-module.js';
 import { doStrFirstCapitalize } from './function-module.js';
 
+const containerElement = document.createElement('div');
+containerElement.classList.add('container');
+document.body.appendChild(containerElement);
+
+const pageLimitSelect = createPageLimitSelect();
+const pageLinksElement = document.createElement('div');
+pageLinksElement.classList.add('page-links');
+
+let currentPage = 1;
+let totalPosts = 0;
+let totalPages = 0;
+let pageLimit = 25;
+
+pageLimitSelect.addEventListener('change', () => {
+  pageLimit = parseInt(pageLimitSelect.value);
+  currentPage = 1;
+  containerElement.innerHTML = '';
+  getPostsWithCommentsAndUser();
+});
+
 async function getPostsWithCommentsAndUser() {
   const userId = new URLSearchParams(location.search).get('user_id');
   let dataForm = 'posts?_expand=user&_embed=comments';
@@ -9,16 +29,23 @@ async function getPostsWithCommentsAndUser() {
     dataForm = `posts?_expand=user&_embed=comments&userId=${userId}`;
   }
   const posts = await getServerData(dataForm);
+  totalPosts = posts.length;
+  totalPages = Math.ceil(totalPosts / pageLimit);
+
   renderView(posts);
+  renderPageLinks();
 }
 
 function renderView(posts) {
-  const containerElement = document.querySelector('.container');
   const ulElement = document.createElement('ul');
   ulElement.classList.add('post-list');
   containerElement.appendChild(ulElement);
 
-  posts.forEach(post => {
+  const startIndex = (currentPage - 1) * pageLimit;
+  const endIndex = currentPage * pageLimit;
+
+  for (let i = startIndex; i < endIndex && i < totalPosts; i++) {
+    const post = posts[i];
     const liElement = document.createElement('li');
     const postLinkElement = document.createElement('a');
     const authorLinkElement = document.createElement('a');
@@ -31,7 +58,71 @@ function renderView(posts) {
 
     liElement.append(postLinkElement, ' ', authorLinkElement);
     ulElement.appendChild(liElement);
+  }
+}
+
+function renderPageLinks() {
+  pageLinksElement.innerHTML = '';
+
+  const previousPageLink = createPageLink('Previous', currentPage > 1, () => {
+    if (currentPage > 1) {
+      currentPage--;
+      containerElement.innerHTML = '';
+      getPostsWithCommentsAndUser();
+    }
   });
+  pageLinksElement.appendChild(previousPageLink);
+
+  for (let page = 1; page <= totalPages; page++) {
+    const pageLinkElement = createPageLink(page.toString(), page !== currentPage, () => {
+      currentPage = page;
+      containerElement.innerHTML = '';
+      getPostsWithCommentsAndUser();
+    });
+    pageLinksElement.appendChild(pageLinkElement);
+  }
+
+  const nextPageLink = createPageLink('Next', currentPage < totalPages, () => {
+    if (currentPage < totalPages) {
+      currentPage++;
+      containerElement.innerHTML = '';
+      getPostsWithCommentsAndUser();
+    }
+  });
+  pageLinksElement.appendChild(nextPageLink);
+
+  containerElement.appendChild(pageLinksElement);
+}
+
+function createPageLimitSelect() {
+  const selectElement = document.createElement('select');
+  selectElement.id = 'page-limit';
+
+  for (let i = 10; i <= 100; i += 10) {
+    const option = document.createElement('option');
+    option.value = i.toString();
+    option.textContent = i.toString();
+    selectElement.appendChild(option);
+  }
+
+  const pageLimitLabel = document.createElement('label');
+  pageLimitLabel.textContent = 'Posts per page:';
+  pageLimitLabel.appendChild(selectElement);
+
+  const pageLimitWrapper = document.createElement('div');
+  pageLimitWrapper.classList.add('page-limit-wrapper');
+  pageLimitWrapper.appendChild(pageLimitLabel);
+
+  document.body.insertBefore(pageLimitWrapper, containerElement);
+  return selectElement;
+}
+
+function createPageLink(text, isEnabled, clickHandler) {
+  const linkElement = document.createElement('button');
+  linkElement.textContent = text;
+  linkElement.disabled = !isEnabled;
+  linkElement.addEventListener('click', clickHandler);
+  return linkElement;
 }
 
 getPostsWithCommentsAndUser();
